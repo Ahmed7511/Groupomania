1ccd05f2e186f3e367c32b74c4b3f607e4e920f5
Ahmed <sidouparadox7@gmail.com>
2020-07-06 00:56:59 +0200

    message

--- backend/Routes/User.js
+++ backend/Routes/User.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/User');
-
+ router.get('/:id', userCtrl.getOneUser);
  router.get('/', userCtrl.getAllUsers);
  router.post('/signup',  userCtrl.signup);
  router.post('/login', userCtrl.login);
